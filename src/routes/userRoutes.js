e81b80b310504ddcfd4b0973a4eb14c59300ee1f
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-11-27 13:49:15 -0300

    rota login add

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,7 @@ const userController = require("../controllers/usuarios");
 const router = express.Router();
 
 router.get("/usuarios", userController.listarUsuarios);
+router.post("/login", userController.login);
 router.post("/usuarios", userController.cadastrar);
 
 module.exports = router;
