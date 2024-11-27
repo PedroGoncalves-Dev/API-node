c013a7ff5578f1e86ba3fe5c476354261a25f975
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-11-27 13:17:25 -0300

    porta .env selecionada

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
   console.log(`server esta rodando na porta ${PORT}`);
